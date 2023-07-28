ce9cfef14a9a1e06fbbc133bc1e1dcb07303aa20
Peng Wang <pengwg@gmail.com>
2023-07-28 16:32:12 -0400

    Add 3 refrence types choice

--- tracks_from_seeds.sh
+++ tracks_from_seeds.sh
@@ -7,6 +7,14 @@ data_path=FUS/
 subject=sub-219-FUS
 session=ses-00
 
+# Choose one of the following reference type which corresponds to different sub-name_seeds_{ref_type}.csv files and reference nifti volumes.
+# treatment: LPS coordinates on the treatment day.
+# AC or PC: LPS coordinates in the AC-PC-Midline coordinate system where AC or PC is (0,0,0)
+
+ref_type='treatment'
+# ref_type='AC'
+# ref_type='PC'
+
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'
@@ -23,11 +31,12 @@ printf "\n${GREEN}Entering $subject/$session/dwi/mrtrix...$NC\n"
 
 
 if [ -d "../../anat" ]; then
-    T1_ACPC_nii=$(find ../../anat \( -name "*T1w_ACPC.nii" -o -name "*T1w_ACPC.nii.gz" \) | head -n 1)
+    REF_nii=$(find ../../anat \( -name "${subject}_${session}_$ref_type.nii" -o -name "${subject}_${session}_$ref_type.nii.gz" \) | head -n 1)
+    echo "$subject_$session_$ref_type.nii"
 fi
     
-if [ -z "$T1_ACPC_nii" ]; then
-    echo -e "${YELLOW}AC-PC aligned T1 volume not found.$NC"
+if [ -z "$REF_nii" ]; then
+    echo -e "${YELLOW}$ref_type reference volume not found.$NC"
     exit 1
 fi
 
@@ -37,18 +46,18 @@ if ! [ -f "mean_b0_preprocessed.nii.gz" ]; then
 fi
 
 if ! [ -f "ACPCtodwi_0GenericAffine.mat" ]; then
-    antsRegistrationSyNQuick.sh -d 3 -t r -f mean_b0_preprocessed.nii.gz -m "$T1_ACPC_nii" -o ACPCtodwi_
-    antsApplyTransforms -d 3 -i "$T1_ACPC_nii"  -o T1_ACPC_coreg.nii.gz -r "$T1_ACPC_nii" -t ACPCtodwi_0GenericAffine.mat
+    antsRegistrationSyNQuick.sh -d 3 -t r -f mean_b0_preprocessed.nii.gz -m "$REF_nii" -o REFtodwi_
+    antsApplyTransforms -d 3 -i "$REF_nii"  -o REF_Volume_coreg.nii.gz -r "$REF_nii" -t REFtodwi_0GenericAffine.mat
 fi
 
-if ! [ -f "../../anat/${subject}_seeds.csv" ]; then
-    echo -e "${YELLOW}${subject}_seeds.csv not found.$NC"
+if ! [ -f "../../anat/${subject}_seeds_$ref_type.csv" ]; then
+    echo -e "${YELLOW}${subject}_seeds_$ref_type.csv not found.$NC"
     exit 1
 fi
 
-antsApplyTransformsToPoints -d 3 -i "../../anat/${subject}_seeds.csv" -o "${subject}_seeds_to_dwi.csv" -t [ACPCtodwi_0GenericAffine.mat, 1]
+antsApplyTransformsToPoints -d 3 -i "../../anat/${subject}_seeds_$ref_type.csv" -o "${subject}_seeds_to_dwi.csv" -t [REFtodwi_0GenericAffine.mat, 1]
 
-exec 3< <(tail -n +2 "../../anat/${subject}_seeds.csv")
+exec 3< <(tail -n +2 "../../anat/${subject}_seeds_$ref_type.csv")
 exec 4< <(tail -n +2 "${subject}_seeds_to_dwi.csv")
 
 while IFS=',' read -r x0 y0 z0 r0 label comment <&3 && IFS=',' read -r x y z r label2 comment2 <&4; do
