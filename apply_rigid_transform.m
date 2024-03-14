80944a95a91006608dd722a7bb5db47eba32c051
Peng Wang <pengwg@gmail.com>
2024-03-14 13:25:02 -0400

    Replaced the matlab script

--- apply_rigid_transform.m
+++ apply_rigid_transform.m
@@ -14,19 +14,8 @@ directional_matrix = info.Transform.T;
 
 ants_transform_data = load(ants_transform);
 
-% Define the rigid transformation matrix
-local_transform = [reshape(ants_transform_data.AffineTransform_double_3_3, [3 4]);
-                   0 0 0 1];
-center_transform = diag([1 1 1 1]);
-center_transform(1:3, 4) = ants_transform_data.fixed;
-center_transform_reverse = diag([1 1 1 1]);
-center_transform_reverse(1:3, 4) = -ants_transform_data.fixed;
-
-rigid_transform = center_transform_reverse * local_transform * center_transform;
-
-% Conversion from LPS to RAS system
-lps2ras = diag([-1, -1, 1, -1]);
-rigid_transform_ras = lps2ras * rigid_transform / lps2ras;
+rigid_transform_ras = ea_antsmat2mat(ants_transform_data.AffineTransform_double_3_3, ...
+                                     ants_transform_data.fixed);
 
 % Apply the rigid transformation by modifying the directional matrix
 new_directional_matrix = directional_matrix * rigid_transform_ras';
@@ -38,4 +27,36 @@ niftiwrite(image_data, output_image, info, 'Compressed', true);
 disp(['Matlab: rigid transformation applied to ' input_image]);
 % fprintf('\x1b[32m%s\x1b[0m\n', ['Rigid transformation applied to ' input_image]);
 
+end
+
+% https://github.com/ANTsX/ANTs/wiki/ITK-affine-transform-conversion
+% https://github.com/InsightSoftwareConsortium/ITK/blob/master/Modules/Core/Transform/include/itkMatrixOffsetTransformBase.hxx
+% https://github.com/netstim/leaddbs/blob/master/helpers/ea_antsmat2mat.m
+
+function mat = ea_antsmat2mat(afftransform, m_Center)
+% followed instructions from
+% https://www.neuro.polymtl.ca/tips_and_tricks/how_to_use_ants and ITK code
+% in ComputeOffset() itkMatrixOffsetTransformBase.hxx
+
+mat=[reshape(afftransform(1:9),[3,3])',afftransform(10:12)];
+
+m_Translation=mat(:,4);
+mat=[mat;[0,0,0,1]];
+
+for i=1:3
+    m_Offset(i) = m_Translation(i) + m_Center(i);
+    for j=1:3
+       m_Offset(i) = m_Offset(i)-(mat(i,j) * m_Center(j));  % (i,j) should remain the same since in C indexing rows before cols, too.
+    end
+end
+
+mat(1:3,4)=m_Offset;
+mat=inv(mat);
+
+% convert RAS to LPS (ITK uses LPS)
+mat=mat.*...
+    [1  1 -1 -1
+     1  1 -1 -1
+    -1 -1  1  1
+     1  1  1  1];
 end
