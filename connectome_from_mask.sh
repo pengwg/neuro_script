308db6e0025f5f00314f682671d9a87833975271
Peng Wang <pengwg@gmail.com>
2024-06-04 16:49:12 -0400

    Done connectome from mask

--- connectome_from_mask.sh
+++ connectome_from_mask.sh
@@ -4,11 +4,13 @@
 
 cores=18 
 
+num_tracks=1M
+
 # Absolute or relative path of the data folder to where the script located
 data_path=~/Work/fusOUD/FUS/sub-224-FUS
 
 # Set to 0 to disable quality control popup
-QC=0
+QC=1
 
 #---------------------------------------------------------------------------
 
@@ -41,70 +43,75 @@ do
         continue
     fi
     
-    if ! [ -d connectome_mask ]; then
-        mkdir connectome_mask
+    if ! [ -f "../../ses-00/anat/${sub_name}_mask_Left_T1.nii.gz" ]; then
+        echo "No mask files found!"
+        cd $basedir
+        continue
     fi
-    cd connectome_mask
     
-    if ! [ -f "aparc+aseg_coreg.nii.gz" ]; then       
+    if ! [ -d connectome_from_mask ]; then
+        mkdir connectome_from_mask
+    fi
+    cd connectome_from_mask
+    
+    if ! [ -f "mask_right_coreg.nii.gz" ]; then
+        antsRegistrationSyNQuick.sh -d 3 -t r -f ../mrtrix4/T1_FS_coreg.nii.gz -m ../../anat/${sub_name}_T1w.nii.gz -o T1w2dwi_ -n $cores
+        
         # Use matlab method to apply image header transformation, avoiding interpolation of image data
         # Requires apply_rigid_transform.m in the script folder
-        matlab -batch "addpath('$basedir'); apply_rigid_transform('../mrtrix3/aparc+aseg.nii.gz', 'aparc+aseg_coreg', '../mrtrix3/FS2dwi_0GenericAffine.mat')"
-        # Also apply the transform to anatomic volume for checking purpose
-        matlab -batch "addpath('$basedir'); apply_rigid_transform('../mrtrix3/T1_FS.nii.gz', 'T1_FS_coreg', '../mrtrix3/FS2dwi_0GenericAffine.mat')"
-        mrconvert T1_FS_coreg.nii.gz T1_FS_coreg.mif -force
+        matlab -batch "addpath('$basedir'); apply_rigid_transform('../../../ses-00/anat/${sub_name}_mask_Left_T1.nii.gz', 'mask_left_coreg', 'T1w2dwi_0GenericAffine.mat')"
+        matlab -batch "addpath('$basedir'); apply_rigid_transform('../../../ses-00/anat/${sub_name}_mask_Right_T1.nii.gz', 'mask_right_coreg', 'T1w2dwi_0GenericAffine.mat')"
+        # mrconvert T1_FS_coreg.nii.gz T1_FS_coreg.mif -force
     fi
 
     # Check registration
     if ! [ $QC -eq 0 ]; then
-        mrview ../mrtrix3/mean_b0_preprocessed.mif -overlay.load T1_FS_coreg.nii.gz -overlay.load aparc+aseg_coreg.nii.gz &
+        mrview ../mrtrix3/mean_b0_preprocessed.mif -overlay.load T1w2dwi_Warped.nii.gz -overlay.load mask_left_coreg.nii.gz -overlay.load mask_right_coreg.nii.gz &
     fi
 
-# Create 5tt registered T1 volume and gray matter/white matter boundary seed using freesurfer segmentation
-    if ! [ -f "5tt_coreg.mif" ]; then
-        # '5ttgen freesurfer' will invoke labelconvert by itself  
-        5ttgen freesurfer aparc+aseg_coreg.nii.gz 5tt_coreg.mif -nthreads $cores
-        
-        # Continue script if 5ttgen failed
-        if ! [ $? -eq 0 ]; then
-            cd $basedir
-            continue
-        fi
-    fi
-    
-    if ! [ $QC -eq 0 ]; then
-        mrview T1_FS_coreg.nii.gz -overlay.load 5tt_coreg.mif &
-    fi
-    
-    if ! [ -f "gmwmSeed_coreg.mif" ]; then
-        5tt2gmwmi 5tt_coreg.mif gmwmSeed_coreg.mif -nthreads $cores -force
-    fi
-    
-    chmod a+x *
-    
-    if ! [ -f "tracks_10M.tck" ]; then
-        tckgen -act 5tt_coreg.mif -backtrack -seed_gmwmi gmwmSeed_coreg.mif -select 10000k ../mrtrix3/wmfod_norm.mif tracks_10M.tck -nthreads $cores
+# Tracks generation from masks
+    if ! [ -f "tracks_${num_tracks}_left.tck" ]; then
+        tckgen -act ../mrtrix4/5tt_coreg.mif -backtrack -seed_image mask_left_coreg.nii.gz -select $num_tracks ../mrtrix3/wmfod_norm.mif tracks_${num_tracks}_left.tck -nthreads $cores
         
         # Clean up and exit if user presses Ctrl->C
         if ! [ $? -eq 0 ]; then
-            rm tracks_10M.tck
+            rm tracks_${num_tracks}_left.tck
             exit 1
         fi
     fi
-    # tckedit tracks_10M.tck -number 200k smallerTracks_200k.tck -force
+    
+    if ! [ -f "tracks_${num_tracks}_right.tck" ]; then
+        tckgen -act ../mrtrix4/5tt_coreg.mif -backtrack -seed_image mask_right_coreg.nii.gz -select $num_tracks ../mrtrix3/wmfod_norm.mif tracks_${num_tracks}_right.tck -nthreads $cores
+        
+        # Clean up and exit if user presses Ctrl->C
+        if ! [ $? -eq 0 ]; then
+            rm tracks_${num_tracks}_right.tck
+            exit 1
+        fi
+    fi
+    # tckedit tracks_$num_tracks.tck -number 200k smallerTracks_200k.tck -force
 
     # mrview ${sub_name}_den_preproc_unbiased.mif -tractography.load smallerTracks_200k.tck
-    if ! [ -f "sift_1M.tck" ]; then
-        tcksift -act 5tt_coreg.mif -term_number 1M tracks_10M.tck ../mrtrix3/wmfod_norm.mif sift_1M.tck -nthreads $cores -force
+    if ! [ -f "sift_1M_left.tck" ]; then
+        tcksift -act ../mrtrix4/5tt_coreg.mif -term_number 1M tracks_${num_tracks}_left.tck ../mrtrix3/wmfod_norm.mif sift_1M_left.tck -nthreads $cores -force
+    fi
+    
+    if ! [ -f "sift_1M_right.tck" ]; then
+        tcksift -act ../mrtrix4/5tt_coreg.mif -term_number 1M tracks_${num_tracks}_right.tck ../mrtrix3/wmfod_norm.mif sift_1M_right.tck -nthreads $cores -force
     fi
 
-    if ! [ -f "sift_10M.txt" ]; then
-        tcksift2 -act 5tt_coreg.mif -out_mu sift_mu.txt -out_coeffs sift_coeffs.txt tracks_10M.tck ../mrtrix3/wmfod_norm.mif sift_10M.txt -nthreads $cores -force
-        tckedit tracks_10M.tck -number 200k smallerTracts_200k.tck
+    if ! [ -f "sift_${num_tracks}_left.txt" ]; then
+        tcksift2 -act ../mrtrix4/5tt_coreg.mif -out_mu sift_mu_left.txt -out_coeffs sift_coeffs_left.txt tracks_${num_tracks}_left.tck ../mrtrix3/wmfod_norm.mif sift_${num_tracks}_left.txt -nthreads $cores -force
+        tckedit tracks_${num_tracks}_left.tck -number 200k smallerTracts_200k_left.tck
+    fi
+    
+    if ! [ -f "sift_${num_tracks}_right.txt" ]; then
+        tcksift2 -act ../mrtrix4/5tt_coreg.mif -out_mu sift_mu_right.txt -out_coeffs sift_coeffs_right.txt tracks_${num_tracks}_right.tck ../mrtrix3/wmfod_norm.mif sift_${num_tracks}_right.txt -nthreads $cores -force
+        tckedit tracks_${num_tracks}_right.tck -number 200k smallerTracts_200k_right.tck
     fi
             
     if ! [ $QC -eq 0 ]; then
-        mrview T1_FS_coreg.nii.gz –tractography.load smallerTracts_200k.tck &
+        mrview ../mrtrix4/T1_FS_coreg.nii.gz –tractography.load smallerTracts_200k_left.tck –tractography.load smallerTracts_200k_right.tck &
     fi
     
     echo -e "${GREEN}${sessions_dir[$n]} ACT done.$NC"
@@ -116,34 +123,17 @@ do
  
                      
     # Connectome with individual freesurfer atlas regions
-    if ! [ -f "${sub_name}_10M_connectome.csv" ]; then
-        labelconvert aparc+aseg_coreg.nii.gz \
-                     $FREESURFER_HOME/FreeSurferColorLUT.txt \
-                     $(dirname $(which mrview))/../share/mrtrix3/labelconvert/fs_default.txt \
-                     fs_parcels_coreg.nii.gz -force
-                     
+    if ! [ -f "${sub_name}_${num_tracks}_connectome_right.csv" ]; then
         tck2connectome -symmetric -zero_diagonal -scale_invnodevol \
-                       -tck_weights_in sift_10M.txt tracks_10M.tck fs_parcels_coreg.nii.gz \
-                       ${sub_name}_10M_connectome.csv \
-                       -out_assignment ${sub_name}_10M_connectome_assignments.csv
+                       -tck_weights_in sift_${num_tracks}_left.txt tracks_${num_tracks}_left.tck ../mrtrix4/fs_parcels_coreg.nii.gz \
+                       ${sub_name}_${num_tracks}_connectome_left.csv \
+                       -out_assignment ${sub_name}_${num_tracks}_connectome_assignments_left.csv
+                       
+        tck2connectome -symmetric -zero_diagonal -scale_invnodevol \
+                       -tck_weights_in sift_${num_tracks}_right.txt tracks_${num_tracks}_right.tck ../mrtrix4/fs_parcels_coreg.nii.gz \
+                       ${sub_name}_${num_tracks}_connectome_right.csv \
+                       -out_assignment ${sub_name}_${num_tracks}_connectome_assignments_right.csv                     
     fi
-    
-    # Connectome scaled with mean FA
-    if ! [ -f "${sub_name}_meanFA_10M_connectome.csv" ]; then 
-        # Computing fractional anisotropy of full 10M track file
-        dwi2tensor ${sub_name}_den_unr_preproc_unbiased.mif tensor.mif -force -nthreads $cores
-        tensor2metric tensor.mif -fa FA.mif -force -nthreads $cores  
-
-        # Computing the mean FA of tracks 
-        tcksample tracks_10M.tck FA.mif tracks_meanFA_10M.csv -stat_tck mean -force -nthreads $cores 
-   
-        tck2connectome -symmetric -zero_diagonal \
-                       -tck_weights_in sift_10M.txt tracks_10M.tck fs_parcels_coreg.nii.gz \
-                       ${sub_name}_meanFA_10M_connectome.csv \
-                       -scale_file tracks_meanFA_10M.csv -stat_edge mean -force 
-    fi
-    
-    chmod a+x *
     
     note="${sessions_dir[$n]} connectome done.$NC  $(date '+%Y-%m-%d %H:%M')" 
     echo -e "$note" >> $basedir/script4BATMAN_log.txt
