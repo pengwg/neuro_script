d08a4b611fa1b78f4161845ffa10972462cf02a4
Peng Wang <pengwg@gmail.com>
2025-11-08 16:34:37 -0500

    Run RCT subject

--- util/dicom_2_bids.sh
+++ util/dicom_2_bids.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-dicom_path="/mnt/msi/Data/naviFUS/dicom"
-out_path="/mnt/msi/Data/naviFUS/nifti"
+dicom_path="/home/pw0032/Data/FUS-RCT/dicom/sub-018-RCT/ses-2-00/"
 
-sub_name=sub-009-NAVI
-session=ses-00
+sub_name=sub-018-RCT
+session=ses-2-00
 
 for d in $(find $dicom_path -type d); do
   if [ -z "$(find "$d" -mindepth 1 -type d)" ]; then
-    dcm2niix -f "${sub_name}_${session}_%d_%s" -p y -z y -o $out_path/$sub_name/$session $d
+    dcm2niix -f "${sub_name}_${session}_%d_%s" -p y -z y -o $dicom_path $d
   fi
 done
 
--- util/run_fs.sh
+++ util/run_fs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-num_threads=16
-data_path="/mnt/msi/Data/naviFUS"
+num_threads=10
+data_path="/home/pw0032/Data/FUS-RCT"
 
-sub_name=sub-009-NAVI
-session=ses-00
+sub_name=sub-018-RCT
+session=ses-2-00
 
 T1_VOL=$(ls $data_path/$sub_name/$session/anat/${sub_name}_${session}_T1*.nii.gz | head -n 1)
 
-recon-all -all -parallel -openmp $num_threads -i $T1_VOL -s $sub_name_$session
+recon-all -all -parallel -openmp $num_threads -i $T1_VOL -s ${sub_name}_$session
